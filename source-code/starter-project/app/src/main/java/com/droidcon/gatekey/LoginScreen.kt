59a1d5a95da6ff0c1b3b60dc5ec4531245049ba6
Somasundaram M <msomasundaram93@gmail.com>
2023-09-30 23:11:38 +0530

    func : starter code and helper files with inital setup for figma

---
+++ source-code/starter-project/app/src/main/java/com/droidcon/gatekey/LoginScreen.kt
@@ -0,0 +1,51 @@
+package com.droidcon.gatekey
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.droidcon.gatekey.logo.Logo
+import com.droidcon.gatekey.signinbuttons.SignInButtons
+import com.droidcon.gatekey.signinfields.SignInFields
+import com.droidcon.gatekey.signinmessages.SignInMessages
+import com.droidcon.gatekey.termsandconditions.TermsAndConditions
+
+@Composable
+fun LoginScreen(
+    modifier: Modifier = Modifier,
+    onSignInButtonTapped: () -> Unit = {},
+    onSignUpButtonTapped: () -> Unit = {},
+) {
+    var email by remember { mutableStateOf("account@gmail.com") }
+    var password by remember { mutableStateOf("password") }
+    Box(modifier.fillMaxSize()) {
+        Column(
+            modifier
+                .fillMaxSize()
+                .padding(vertical = 64.dp),
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Logo(modifier = Modifier.fillMaxWidth())
+            SignInMessages(modifier = Modifier.fillMaxWidth())
+            SignInFields(
+                modifier = Modifier.fillMaxWidth(),
+            )
+            TermsAndConditions(
+                modifier = Modifier.fillMaxWidth(),
+                description = "I agree to the Terms and conditions"
+            )
+            SignInButtons(
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+    }
+}
---
+++ source-code/starter-project/app/src/main/java/com/droidcon/gatekey/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.droidcon.gatekey.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF42DCD0)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFF66F8EC)
+
+val Purple40 = Color(0xFF006A64)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF00504B)
